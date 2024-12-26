504e0cf4dfb049f11c0fdfa13c1b2daf2e908282
LanceBongon <lance.bongon@outlook.com>
2024-12-27 04:17:35 +1100

    Added more element refferences.

--- assets/app/UI_Refferences/AccountUI.js
+++ assets/app/UI_Refferences/AccountUI.js
@@ -1,23 +1,36 @@
 class AccountUI {
    #html_dom_purify = DOMPurify
-   accounts_element_refference = {
-      // input_call_connid: document.getElementById(""),
-      // input_billing_account_number: document.getElementById(""),
-      // input_customer_name: document.getElementById(""),
+   #accounts_element_refference = {
+      input_call_connection_id: document.getElementById("input-call-connid"),
+      input_billing_account_number: document.getElementById("input-billing-account-number"),
+      input_customer_name: document.getElementById("input-customer-name"),
+
       input_amount_payable: document.getElementById("input-amount-payable"),
       input_overdue_balance: document.getElementById("input-overdue-balance"),
       input_upcoming_balance: document.getElementById("input-upcoming-balance")
    }
 
+   constructor() {
 
+   }
+
+   set connection_id(value) {
+      const sanitized_value = this.#html_dom_purify.sanitize(value);
+      this.#accounts_element_refference.input_call_connection_id.value = sanitized_value;
+   }
+   set billing_account_number(value){
+      const sanitized_value = this.#html_dom_purify.sanitize(value);
+      this.#accounts_element_refference.input_billing_account_number.value = sanitized_value;
+   }
    set amount_payable(value) {
       const sanitized_value = this.#html_dom_purify.sanitize(value);
-      this.accounts_element_refference.input_amount_payable.value = parseFloat(sanitized_value);
+      this.#accounts_element_refference.input_amount_payable.value = parseFloat(sanitized_value);
    }
    set overdue_balance(value) {
       const sanitized_value = this.#html_dom_purify.sanitize(value);
-      this.accounts_element_refference.input_overdue_balance.value = parseFloat(sanitized_value);
+      this.#accounts_element_refference.input_overdue_balance.value = parseFloat(sanitized_value);
    }
+
 
 }
 
@@ -34,9 +47,11 @@ class AccountUITest {
    }
 
    test_accountui_value() {
+      this.#account_refference.connection_id = "123jndfae";
+      this.#account_refference.billing_account_number = 12314253123;
       this.#account_refference.amount_payable = 100;
       this.#account_refference.overdue_balance = 50
-
    }
 
 }
+
